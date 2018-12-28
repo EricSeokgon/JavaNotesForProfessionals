e6d83e633465688ddac44c7b2d2f840b58dec1d3
hadesyi <leesk55@gmail.com>
2018-12-28 17:53:36 +0900

    add Member class

---
+++ src/chapter1/Member.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class Member {
+    private String name;
+    private String type;
+    private int level;
+    private int rank;
+
+    public Member(String name, String type, int level, int rank) {
+        this.name = name;
+        this.type = type;
+        this.level = level;
+        this.rank = rank;
+    }
+}
