f9cdca1610368be0405ef4ec81579a88fbd57969
hadesyi <leesk55@gmail.com>
2019-01-18 08:57:50 +0900

    Using underscore to improve readability

--- src/chapter9/Literals.java
+++ src/chapter9/Literals.java
@@ -5,5 +5,12 @@ public class Literals {
         int i1 = 123456;
         int i2 = 123_456;
         System.out.println(i1 == i2); //true
+
+        byte color = 1_2_3;
+        short yearsAAnnoDomini = 2_019;
+        int socialSecurtyNumber = 999_99_9999;
+        long creditCardNumber = 1234_5678_9012_3456L;
+        float piFoutDecimals = 3.14_15F;
+        double piTenDecimals = 3.14_15_92_65_35;
     }
 }
