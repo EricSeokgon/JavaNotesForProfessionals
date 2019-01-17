c44421ce4de5daab983ac34046ca9f158c5fa100
hadesyi <leesk55@gmail.com>
2019-01-17 09:05:07 +0900

    For instance, these two declarations are equivalent

---
+++ src/chapter9/Literals.java
@@ -0,0 +1,9 @@
+package chapter9;
+
+public class Literals {
+    public static void main(String[] args) {
+        int i1 = 123456;
+        int i2 = 123_456;
+        System.out.println(i1 == i2); //true
+    }
+}
