cfd59fbe68622c2cddb5b9b9031babe0d83f7573
hadesyi <leesk55@gmail.com>
2019-01-21 10:06:19 +0900

    Changing the case of characters within a String

---
+++ src/chapter11/ChangingTheCaseOfCharacters.java
@@ -0,0 +1,14 @@
+package chapter11;
+
+public class ChangingTheCaseOfCharacters {
+    public static void main(String[] args) {
+        String s = "This is a Random String.";
+
+        String s1 = s.toUpperCase();
+        String s2 = s.toLowerCase();
+
+        System.out.println(s);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
