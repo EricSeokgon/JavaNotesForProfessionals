3462a2f0cae0f525fa836215dba5e2ad31088f61
hadesyi <leesk55@gmail.com>
2019-03-07 09:11:33 +0900

    Changing the case of characters within a String

---
+++ src/chapter12/Exam2.java
@@ -0,0 +1,30 @@
+package chapter12;
+
+import java.util.Scanner;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String s = scanner.next();
+        char[] a = s.toCharArray();
+        System.out.println("Enter the character you are looking for");
+        System.out.println(s);
+        String c = scanner.next();
+        char d = c.charAt(0);
+        for (int i = 0; i <= s.length(); i++) {
+            if (a[i] == d) {
+                if (d >= 'a'
+                        && d <= 'z') {
+                    d -= 32;
+                } else if (d >= 'A' && d <= 'Z') {
+                    d += 32;
+                }
+                a[i] = d;
+                break;
+            }
+        }
+        s = String.valueOf(a);
+        System.out.println(s);
+    }
+}
