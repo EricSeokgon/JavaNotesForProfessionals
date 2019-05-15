010783eba55fe2e37ce413fa53cfb6a9b319ab85
hadesyi <leesk55@gmail.com>
2019-05-15 11:25:19 +0900

    To convert long or int values to BigInteger use

---
+++ src/chapter20/BigInteger.java
@@ -0,0 +1,11 @@
+package chapter20;
+
+public class BigInteger {
+
+    String decimalString = "-1";
+    BigInteger valueFromDecimalString = new BigInteger(decimalString);
+
+    public BigInteger(String decimalString) {
+
+    }
+}
