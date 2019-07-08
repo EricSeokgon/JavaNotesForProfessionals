781e6a3f850d1acc070e35f6c8f5a0e0f8192f0b
hadesyi <leesk55@gmail.com>
2019-07-08 11:44:58 +0900

    Creating and Initializing Arrays

---
+++ src/chapter23/ArraysTest.java
@@ -0,0 +1,10 @@
+package chapter23;
+
+public class ArraysTest {
+    int[] numbers1 = new int[3]; // Array for 3 int values, default value is 0
+    int[] numbers2 = { 1, 2, 3 }; // Array literal of 3 int values
+    int[] numbers3 = new int[] { 1, 2, 3 }; // Array of 3 int values initialized
+    int[][] numbers4 = { { 1, 2 }, { 3, 4, 5 } }; // Jagged array literal
+    int[][] numbers5 = new int[5][]; // Jagged array, one dimension 5 long
+    int[][] numbers6 = new int[5][4]; // Multidimensional array: 5x4
+}
