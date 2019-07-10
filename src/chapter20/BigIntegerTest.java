69fca5aa49540d867866672ecb01627e9f3c0c91
hadesyi <leesk55@gmail.com>
2019-07-10 10:07:16 +0900

    Creating a List from an Array

--- src/chapter20/BigIntegerTest.java
+++ src/chapter20/BigIntegerTest.java
@@ -1,6 +1,8 @@
 package chapter20;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
 
 public class BigIntegerTest {
     public static void main(String[] args) {
@@ -30,28 +32,25 @@ public class BigIntegerTest {
 
         BigInteger one = BigInteger.valueOf(1);
         BigInteger two = BigInteger.valueOf(2);
-        if(one.equals(two)){
+        if (one.equals(two)) {
             System.out.println("Equal");
-        }
-        else{
+        } else {
             System.out.println("Not Equal");
         }
 
 
         BigInteger reallyBig = BigInteger.valueOf(1);
-        if(BigInteger.ONE.equals(reallyBig)){
-        //code when they are equal.
+        if (BigInteger.ONE.equals(reallyBig)) {
+            //code when they are equal.
         }
 
         BigInteger reallyBig1 = BigInteger.valueOf(100);
-        if(reallyBig.compareTo(reallyBig1) == 0){
-        //code when both are equal.
-        }
-        else if(reallyBig.compareTo(reallyBig1) == 1){
-        //code when reallyBig is greater than reallyBig1.
-        }
-        else if(reallyBig.compareTo(reallyBig1) == -1){
-        //code when reallyBig is less than reallyBig1.
+        if (reallyBig.compareTo(reallyBig1) == 0) {
+            //code when both are equal.
+        } else if (reallyBig.compareTo(reallyBig1) == 1) {
+            //code when reallyBig is greater than reallyBig1.
+        } else if (reallyBig.compareTo(reallyBig1) == -1) {
+            //code when reallyBig is less than reallyBig1.
         }
 
         BigInteger val1 = new BigInteger("10");
@@ -64,6 +63,8 @@ public class BigIntegerTest {
         val1.not();
         val1.andNot(val2);
 
-    }
+        String[] stringArray = {"foo", "bar", "baz"};
+        List<String> stringList = Arrays.asList(stringArray);
 
+    }
 }
