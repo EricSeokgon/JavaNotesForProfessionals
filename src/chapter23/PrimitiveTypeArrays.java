23ed7a2cec806fc5447de86165e5fd3b203d058d
hadesyi <leesk55@gmail.com>
2019-07-19 09:40:26 +0900

    Creating and initializing primitive type arrays

---
+++ src/chapter23/PrimitiveTypeArrays.java
@@ -0,0 +1,11 @@
+package chapter23;
+
+public class PrimitiveTypeArrays {
+    public static void main(String[] args) {
+        int[] array1 = new int[] { 1, 2, 3 }; // Create an array with new operator and
+                                            // array initializer.
+        int[] array2 = { 1, 2, 3 }; // Shortcut syntax with array initializer.
+        int[] array3 = new int[3]; // Equivalent to { 0, 0, 0 }
+        int[] array4 = null; // The array itself is an object, so it
+    }
+}
