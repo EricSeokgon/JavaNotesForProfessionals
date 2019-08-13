159d95f1e0c493a0a0430ddf0b193ca1c32e2527
hadesyi <leesk55@gmail.com>
2019-08-13 11:44:21 +0900

    MovieThread 생성

--- src/sec01/verify/exam02/ThreadExample.java
+++ src/sec01/verify/exam02/ThreadExample.java
@@ -5,6 +5,9 @@ public class ThreadExample {
         Thread thread1 = new MovieThread();
         thread1.start();
 
+        Thread thread2 = new Thread(new MusicRunnable());
+        thread2.start();
+
 
     }
 }
