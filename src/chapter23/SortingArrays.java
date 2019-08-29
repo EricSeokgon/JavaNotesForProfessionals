c62833847b23135df4d6f63da8217a81fbd036d6
hadesyi <leesk55@gmail.com>
2019-08-29 14:52:09 +0900

    Sorting arrays

---
+++ src/chapter23/SortingArrays.java
@@ -0,0 +1,11 @@
+package chapter23;
+
+import java.util.Arrays;
+
+public class SortingArrays {
+    public static void main(String[] args) {
+        int[] array = {7, 4, 2, 1, 19};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
