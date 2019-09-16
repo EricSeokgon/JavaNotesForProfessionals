d90fd8f1c781ebcfd0f7834106c68765e6a4f3fa
hadesyi <leesk55@gmail.com>
2019-09-16 09:33:53 +0900

    Iterating over Collections

--- src/chapter24/Iterating.java
+++ src/chapter24/Iterating.java
@@ -7,8 +7,18 @@ import java.util.List;
 public class Iterating {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>(Arrays.asList("Clementine", "Duran", "Mike"));
-        //java SE 8
+        //Version >= java SE 8
         names.forEach(System.out::println);
+
+        names.parallelStream().forEach(System.out::println);
+        //Version >= Java SE 5
+        for (String name : names) {
+            System.out.println(name);
+        }
+        //Version <= Java SE 5
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
 
     }
 }
