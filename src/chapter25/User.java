a9f1c02d3083c6dce2d7d94ac15d80c1268d59f2
hadesyi <leesk55@gmail.com>
2019-09-23 09:24:01 +0900

    Sorting a generic list

--- src/chapter25/User.java
+++ src/chapter25/User.java
@@ -1,14 +1,21 @@
 package chapter25;
 
-public class User {
+public class User implements Comparable<User> {
     public final Long id;
     public final String username;
+
     public User(Long id, String username) {
         this.id = id;
         this.username = username;
     }
+
     @Override
     public String toString() {
         return String.format("%s:%d", username, id);
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return id.compareTo(o.id);
     }
 }
