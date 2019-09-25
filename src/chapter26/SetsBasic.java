9924892c43386ad550dfda8bdbfb315e1358c021
hadesyi <leesk55@gmail.com>
2019-09-25 10:27:06 +0900

    Types and Usage of Sets

--- src/chapter26/SetsBasic.java
+++ src/chapter26/SetsBasic.java
@@ -1,6 +1,7 @@
 package chapter26;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetsBasic {
@@ -13,6 +14,21 @@ public class SetsBasic {
         System.out.println(set);
         set.clear(); //Removes all objects from the collection.
         System.out.println(set);
+
+        //HashSet - Random Sorting
+        Set<String> setStr = new HashSet<>();
+        setStr.add("Banana");
+        setStr.add("Banana");
+        setStr.add("Apple");
+        setStr.add("Strawberry");
+        System.out.println(setStr);
+        //LinkedHashSet - Insertion Order
+        Set<String> setStr2 = new LinkedHashSet<>();
+        setStr2.add("Banana");
+        setStr2.add("Banana");
+        setStr2.add("Apple");
+        setStr2.add("Strawberry");
+        System.out.println(setStr2);
 
     }
 }
