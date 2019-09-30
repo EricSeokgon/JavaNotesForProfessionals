638da7a20692acd7cfb1e2a5327b3e05706667c9
hadesyi <leesk55@gmail.com>
2019-09-30 11:45:22 +0900

    List vs Set

--- src/chapter27/SetAndListExample.java
+++ src/chapter27/SetAndListExample.java
@@ -1,18 +1,28 @@
 package chapter27;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class SetAndListExample {
     public static void main(String[] args) {
+
         System.out.println("List example .....");
+
         List list = new ArrayList();
         list.add("1");
         list.add("2");
         list.add("3");
         list.add("4");
         list.add("1");
-        for (String temp : list){
+
+        for (Object temp : list){
             System.out.println(temp);
         }
+
         System.out.println("Set example .....");
+
         Set<String> set = new HashSet<String>();
         set.add("1");
         set.add("2");
@@ -21,8 +31,10 @@ public class SetAndListExample {
         set.add("1");
         set.add("2");
         set.add("5");
+
         for (String temp : set){
             System.out.println(temp);
         }
+
     }
 }
